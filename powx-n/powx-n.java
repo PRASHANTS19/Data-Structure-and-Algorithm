d7a4efd3281a91e3a7cdd436f5be5b52d5bc5e42
prahanat0029 <95503105+prahanat0029@users.noreply.github.com>
2021-12-29 12:13:53 +0530

    Time: 2604 ms (6.96%), Space: 40.1 MB (7.25%) - LeetHub

--- powx-n/powx-n.java
+++ powx-n/powx-n.java
@@ -1,5 +1,24 @@
 class Solution {
     public double myPow(double x, int n) {
-        return Math.pow(x,n);
+      if(n==0 || x==1)return 1;
+      if (x == -1) {
+            if (n % 2 == 0) return 1;
+            else return -1;
+        }
+        double ans = 1;
+        if (n == Integer.MIN_VALUE) return 0;
+        if(n>0){
+            while(n>0){
+                ans = ans*x;
+                n--;
+            }
+        }
+        else{
+            while(n<0){
+                ans = ans/x;
+                n++;
+            }
+        }
+        return ans;
     }
 }
