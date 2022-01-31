92f2b45a70c01828b0ab6edbf699a8dbd0d20ed6
Prashant sahu <95503105+PRASHANTS19@users.noreply.github.com>
2022-01-31 08:23:49 +0530

    Time: 18 ms (91.94%), Space: 46.5 MB (52.56%) - LeetHub

--- 15-3sum/15-3sum.java
+++ 15-3sum/15-3sum.java
@@ -3,23 +3,23 @@ class Solution {
         List<List<Integer>> matrix = new LinkedList<>();
         Arrays.sort(arr);
         if(arr.length==0)return matrix;
-        for(int i=0; i<arr.length-2; i++){
-            if(i>0 && arr[i]==arr[i-1])continue; 
-            //help to remove the dup to enter in the list.
-            int j=i+1; int k=arr.length-1;
+        int n = arr.length;
+        for(int i=0; i<n-2; i++){
+            int j=i+1; int k=n-1;
             while(j<k){
-                if(arr[j]+arr[k]==-arr[i]){
+                if(arr[i]+arr[j]+arr[k]==0){
                     matrix.add(Arrays.asList(arr[i],arr[j],arr[k]));
+                    //can remove duplicacy by skipping duplicates
                     while(j<k && arr[j]==arr[j+1])j++;
                     while(j<k && arr[k]==arr[k-1])k--;
                     j++; k--;
                 }
-                else if(arr[j]+arr[k]>-arr[i]){
-                    k--;
-                }
+                else if(arr[i]+arr[j]+arr[k]>0)k--;
                 else j++;
             }
+            while(i<n-2 && arr[i]==arr[i+1])i++;
         }
         return matrix;
+        
     }
 }
