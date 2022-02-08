74cbfd86c73d762c521ccbd264c69f3f771d789c
Prashant sahu <95503105+PRASHANTS19@users.noreply.github.com>
2022-02-09 00:32:29 +0530

    Time: 22 ms (25.31%), Space: 42.6 MB (24.31%) - LeetHub

---
+++ 38-count-and-say/38-count-and-say.java
@@ -0,0 +1,30 @@
+class Solution {
+    public String countAndSay(int n) {
+        
+        if(n==1) return "1";
+        if(n==2) return "11";
+        
+      
+        String s = "11";
+        
+        for(int i=3; i<=n; i++){
+            int count = 1;
+            String t = "";
+            for(int j=1; j<s.length(); j++){
+                if(s.charAt(j) != s.charAt(j-1)){
+                    t = t + String.valueOf(count);
+                    t = t + s.charAt(j-1);
+                    count = 1;
+                }
+                else{
+                    count++;
+                }
+            }
+            t = t + String.valueOf(count);
+            t = t + s.charAt(s.length()-1);
+            
+            s = t;
+        }
+        return s;
+     }
+}
