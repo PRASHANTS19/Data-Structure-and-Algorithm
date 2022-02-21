b98a10b6554f685a2088e9a9abe2fd616fa2472c
Prashant sahu <95503105+PRASHANTS19@users.noreply.github.com>
2022-02-21 13:06:27 +0530

    Time: 2 ms (50.88%), Space: 41.2 MB (34.93%) - LeetHub

--- 14-longest-common-prefix/14-longest-common-prefix.java
+++ 14-longest-common-prefix/14-longest-common-prefix.java
@@ -4,25 +4,18 @@ class Solution {
         if(n==0)return "";
         if(n==1)return arr[0];
         
-        String common = "";
-        int i=0; int j=0;
-        while(i<arr[0].length() && j<arr[1].length()){
-            if(arr[0].charAt(i)==arr[1].charAt(j))common += arr[0].charAt(i);
-            else break;
-            i++; j++;
-        }
-        i=0;
-       
-        for(i=2; i<n; i++){
-            String res = "";
-            j=0; int k=0;
-            while(j<arr[i].length() && k<common.length()){
-                if(arr[i].charAt(j)==common.charAt(k))res += arr[i].charAt(j);
-                j++; k++;
+        StringBuilder res = new StringBuilder();
+        Arrays.sort(arr);
+        String first = arr[0];
+        String last = arr[arr.length-1];
+        
+        for(int i=0; i<first.length(); i++){
+            if(first.charAt(i)==last.charAt(i)){
+                res.append(first.charAt(i));
             }
-            common = res;
+            else break;
         }
-    return common;        
+    return res.toString();        
         
     }
 }
