95fbb288c894d139978a86da41ea3a890ba7d81d
Prashant sahu <95503105+PRASHANTS19@users.noreply.github.com>
2022-02-23 16:26:01 +0530

    Time: 1 ms (96.72%), Space: 45 MB (19.53%) - LeetHub

--- 152-maximum-product-subarray/152-maximum-product-subarray.java
+++ 152-maximum-product-subarray/152-maximum-product-subarray.java
@@ -1,43 +1,28 @@
 class Solution {
     public int maxProduct(int[] arr) {
         int n=arr.length;
-        int dp1[] = new int[n];
-        Arrays.fill(dp1 , 1);
-        int dp2[] = new int[n];
-        Arrays.fill(dp2, 1);
-        
         if(n==1)return arr[0];
-        
-        dp1[0] = arr[0];
-        
-        dp2[n-1] = arr[n-1];
-        int max = Integer.MIN_VALUE;
-        
+        int prev = arr[0];
+        int curr = 1;
+        int max = prev;
         for(int i=1; i<n; i++){
-            if(arr[i]==0)dp1[i]=0;
-            else{
-                if(dp1[i-1]!=0){
-                    dp1[i] = arr[i]*dp1[i-1];
-                }
-                else{
-                    dp1[i] = arr[i];
-                }
-            }
+            if(prev!=0)
+                curr = prev*arr[i];
+            else
+                curr = arr[i];
+            prev= curr;
+            max = Math.max(max,curr);
         }
-        for(int i=n-2; i>=0; i--){
-            if(arr[i]==0)dp2[i]=0;
-            else{
-                if(dp2[i+1]!=0){
-                    dp2[i] = arr[i]*dp2[i+1];
-                }
-                else{
-                    dp2[i] = arr[i];
-                }
-            }
+        prev = arr[n-1];
+        max = Math.max(max,prev);
+        for(int i=n-2; i>0; i--){
+            if(prev!=0)
+                curr = prev*arr[i];
+            else
+                curr = arr[i];
+            prev= curr;
+            max = Math.max(max,curr);
         }
-        for(int i=0; i<n; i++){
-               max = Math.max(Math.max(dp1[i],dp2[i]),max);
-            }
-         return max;
+        return max;
     }
 }
