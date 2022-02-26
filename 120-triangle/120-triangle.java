35f8bb144de860c4da8ec4f72a22afe7e64265c6
Prashant sahu <95503105+PRASHANTS19@users.noreply.github.com>
2022-02-26 12:50:28 +0530

    Time: 1 ms (99.80%), Space: 41.7 MB (50.85%) - LeetHub

---
+++ 120-triangle/120-triangle.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int minimumTotal(List<List<Integer>> arr) {
+        int m = arr.size();
+        // if(m==1)return arr.get(0).get(0);
+        // int ans = Integer.MAX_VALUE;
+        
+        int dp[][] = new int[m][m];  
+        for(int x[] : dp){
+            Arrays.fill(x,-1);
+        }
+        
+        return solve(arr,0,0,m-1,dp);
+        
+    }
+    int solve(List<List<Integer>> arr,int i,int j,int m,int dp[][]){
+        if(i==m-1 && j==arr.get(m).size()-1)return arr.get(i).get(j);
+        if(i>m || j>=arr.get(m).size())return 0;
+        
+        if(dp[i][j]!=-1)return dp[i][j];
+        
+        int x1 = solve(arr,i+1,j,m,dp);
+        int x2 = solve(arr,i+1,j+1,m,dp);
+        
+        return dp[i][j]=arr.get(i).get(j)+Math.min(x1,x2);
+    }
+}
