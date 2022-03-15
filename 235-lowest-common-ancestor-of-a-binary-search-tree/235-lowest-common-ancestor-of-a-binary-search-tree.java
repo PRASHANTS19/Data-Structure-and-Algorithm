9576e2af97dc1342715969d5deab5e685d498ae3
Prashant sahu <95503105+PRASHANTS19@users.noreply.github.com>
2022-03-15 13:38:05 +0530

    Time: 6 ms (70.89%), Space: 50.4 MB (15.16%) - LeetHub

--- 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.java
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.java
@@ -10,13 +10,26 @@
 
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if(root==null || root==p || root==q)return root;
+//         if(root==null || root==p || root==q)return root;
         
-        TreeNode left = lowestCommonAncestor(root.left,p,q);
-        TreeNode right = lowestCommonAncestor(root.right,p,q);
+//         TreeNode left = lowestCommonAncestor(root.left,p,q);
+//         TreeNode right = null;
+//         if(left!=null && )
+//         TreeNode right = lowestCommonAncestor(root.right,p,q);
         
-        if(left==null)return right;
-        else if(right==null)return left;
-        return root;
+//         if(left==null)return right;
+//         else if(right==null)return left;
+//         return root;
+        
+        return solve( root,  p,  q);
     }    
+    TreeNode solve(TreeNode root, TreeNode p, TreeNode q){
+        if(root.val>p.val && root.val>q.val){
+            return solve(root.left,p,q);
+        }
+        else if(root.val<p.val && root.val<q.val){
+            return solve(root.right,p,q);
+        }
+        return root;
+    }
 }
