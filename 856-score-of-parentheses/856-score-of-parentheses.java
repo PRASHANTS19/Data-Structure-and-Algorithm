af5e426ea8f07499a3db5e63d4fb1bddfa0ec842
Prashant sahu <95503105+PRASHANTS19@users.noreply.github.com>
2022-03-17 09:42:17 +0530

    Time: 3 ms (5.00%), Space: 41.4 MB (63.60%) - LeetHub

--- 856-score-of-parentheses/856-score-of-parentheses.java
+++ 856-score-of-parentheses/856-score-of-parentheses.java
@@ -5,8 +5,7 @@ class Solution {
         
         stack.push(new pair('(',0));
         map.put(0,0);
-        
-        int count = 0;
+      
         for(int i=1; i<s.length(); i++){
             if(s.charAt(i)=='('){
                 stack.push(new pair('(',i));
@@ -15,13 +14,12 @@ class Solution {
             else if(s.charAt(i)==')'){
                 if(stack.peek().symbol=='(' && stack.peek().index+1==i){
                     map.put(i,map.get(i-1)+1);
-                    stack.pop();
-                    // count += 1;
+                    stack.pop();    
                 }
                 else if(stack.peek().symbol=='('){
-                    map.put(i,map.get(stack.peek().index)+(map.get(i-1)-map.get(stack.peek().index))*2);
+                    int x = map.get(stack.peek().index)+(map.get(i-1)-map.get(stack.peek().index))*2;
+                    map.put(i,x);
                     stack.pop();
-                    // count = count*2;
                 }
             }
         }
