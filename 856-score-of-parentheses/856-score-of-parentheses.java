33d9ec3347b9bc86b7c283061d389bbf60045eda
Prashant sahu <95503105+PRASHANTS19@users.noreply.github.com>
2022-03-17 09:45:00 +0530

    Time: 1 ms (68.54%), Space: 42.1 MB (24.45%) - LeetHub

--- 856-score-of-parentheses/856-score-of-parentheses.java
+++ 856-score-of-parentheses/856-score-of-parentheses.java
@@ -1,37 +1,16 @@
 class Solution {
     public int scoreOfParentheses(String s) {
-        Stack<pair> stack = new Stack<>();
-        HashMap<Integer,Integer> map = new HashMap<>();
-        
-        stack.push(new pair('(',0));
-        map.put(0,0);
-      
-        for(int i=1; i<s.length(); i++){
-            if(s.charAt(i)=='('){
-                stack.push(new pair('(',i));
-                map.put(i,map.get(i-1));
+        Stack<Integer> stack = new Stack<>();
+        int count = 0;
+        for(char c : s.toCharArray()){
+            if(c=='('){
+                stack.push(count);
+                count = 0;
             }
-            else if(s.charAt(i)==')'){
-                if(stack.peek().symbol=='(' && stack.peek().index+1==i){
-                    map.put(i,map.get(i-1)+1);
-                    stack.pop();    
-                }
-                else if(stack.peek().symbol=='('){
-                    int x = map.get(stack.peek().index)+(map.get(i-1)-map.get(stack.peek().index))*2;
-                    map.put(i,x);
-                    stack.pop();
-                }
+            else{
+                count = stack.pop() + Math.max(count*2,1);
             }
         }
-        return map.get(s.length()-1);
-    }
-}
-class pair{
-    char symbol;
-    int index;
-    
-    pair(char c , int i){
-        symbol = c;
-        index = i;
-    }
+        return count;
+    }  
 }
