e53a8554f7e15bd770b170f354d99210060be793
Prashant sahu <95503105+PRASHANTS19@users.noreply.github.com>
2022-03-29 09:25:48 +0530

    Added solution - LeetHub

---
+++ Detect cycle in a directed graph - GFG/detect-cycle-in-a-directed-graph.java
@@ -0,0 +1,71 @@
+// { Driver Code Starts
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class DriverClass {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            ArrayList<ArrayList<Integer>> list = new ArrayList<>();
+            int V = sc.nextInt();
+            int E = sc.nextInt();
+            for (int i = 0; i < V + 1; i++)
+                list.add(i, new ArrayList<Integer>());
+            for (int i = 0; i < E; i++) {
+                int u = sc.nextInt();
+                int v = sc.nextInt();
+                list.get(u).add(v);
+            }
+            if (new Solution().isCyclic(V, list) == true)
+                System.out.println("1");
+            else
+                System.out.println("0");
+        }
+    }
+}// } Driver Code Ends
+
+
+/*Complete the function below*/
+
+class Solution {
+    // Function to detect cycle in a directed graph.
+    public boolean isCyclic(int V, ArrayList<ArrayList<Integer>> adj) {
+        // code here
+        boolean visited[] = new boolean[V];
+        boolean dfsvis[] = new boolean[V];
+        
+        for(int i=0; i<V; i++){
+            if(visited[i]==false){
+                if(dfs(adj,i,visited,dfsvis))return true;
+            }
+        }
+        return false;
+       
+    }
+    boolean dfs(ArrayList<ArrayList<Integer>> adj,int start,boolean visited[],boolean[] dfsvis){
+        visited[start]=true;
+        dfsvis[start]=true;
+        
+        for(int u : adj.get(start)){
+            if(visited[u]==false){
+                if(dfs(adj,u,visited,dfsvis))return true;
+            }
+            else if(dfsvis[u]==true)return true;
+        }
+        dfsvis[start]=false;
+        return false;
+    }
+    
+}
+class Node{
+    int first;
+    int second;
+    
+    Node(int f,int s){
+        first = f;
+        second = s;
+    }
+}
