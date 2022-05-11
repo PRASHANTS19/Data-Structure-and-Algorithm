b0a3a0be2adfa40c51f005b09c8445c8d1ffb099
Prashant sahu <95503105+PRASHANTS19@users.noreply.github.com>
2022-05-12 02:19:20 +0530

    Time: 0 ms (100.00%), Space: 41.1 MB (29.38%) - LeetHub

--- 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.java
+++ 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.java
@@ -1,16 +1,24 @@
 class Solution {
     public int countVowelStrings(int n) {
         
-        int dp[][] = new int[n+1][6];
-        
-        for(int i=1; i<=n; i++){
-            for(int j=1; j<=5; j++){
-                if(i>1)
-                    dp[i][j] =  dp[i-1][j]+dp[i][j-1];
-                else 
-                    dp[i][j] = 1+dp[i][j-1];
+        int dp[][] = new int[n][5];
+        for(int i=0; i<n; i++){
+            for(int j=0; j<5; j++){
+                dp[i][j]=-1;
             }
         }
-        return dp[n][5];
+        
+        return solve(4,n,0,dp);
+    }
+    int solve(int i,int n,int count,int[][] dp){
+        if(i<0)return 0;
+        if(count==n)return 1;
+        
+        if(dp[count][i]!=-1)return dp[count][i];
+        
+        int select = solve(i,n,count+1,dp);
+        int notselect = solve(i-1,n,count,dp);
+        
+        return dp[count][i]=select+notselect;
     }
 }
