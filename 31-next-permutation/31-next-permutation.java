741023935c5b8f5431c6e5a008cecc126030d49e
Prashant sahu <95503105+PRASHANTS19@users.noreply.github.com>
2022-05-16 16:21:16 +0530

    Time: 1 ms (83.13%), Space: 43.3 MB (72.95%) - LeetHub

--- 31-next-permutation/31-next-permutation.java
+++ 31-next-permutation/31-next-permutation.java
@@ -1,33 +1,33 @@
 class Solution {
     public void nextPermutation(int[] arr) {
-        int pivot = -1;
         int n = arr.length;
+        int pivot = -1;
         for(int i=n-2; i>=0; i--){
-            if(arr[i+1]>arr[i]){
+            if(arr[i]<arr[i+1]){
                 pivot = i;
                 break;
             }
         }
-        if(pivot == -1)Arrays.sort(arr);
+        
+        if(pivot==-1)Arrays.sort(arr);
         else{
             for(int i=n-1; i>=0; i--){
                 if(arr[i]>arr[pivot]){
-                    int x = arr[pivot];
-                    arr[pivot] = arr[i];
-                    arr[i] = x;
+                    int temp = arr[i];
+                    arr[i] = arr[pivot];
+                    arr[pivot] = temp;
                     break;
                 }
             }
-        
-        int i = pivot+1;
-        int j = n-1;
-        while(i<j){
-            int x = arr[j];
-            arr[j] = arr[i];
-            arr[i] = x;
-            i++; j--;
+            int i=pivot+1;
+            int j=n-1;
+            
+            while(i<=j){
+                int temp = arr[j];
+                arr[j] = arr[i];
+                arr[i] = temp;
+                i++; j--;
+            }
         }
-        }
-        
     }
 }
