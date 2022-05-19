5c48cbc3448215376274ad2cfe00dd331ad69666
Prashant sahu <95503105+PRASHANTS19@users.noreply.github.com>
2022-05-20 00:01:13 +0530

    Time: 14 ms (40.68%), Space: 53.8 MB (56.37%) - LeetHub

--- 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.java
+++ 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.java
@@ -1,23 +1,30 @@
 class Solution {
+    int res=0;
+    int dp[][];
     public int longestIncreasingPath(int[][] matrix) {
-        if (matrix==null || matrix.length==0 || matrix[0].length==0) return 0;
-        int m=matrix.length, n=matrix[0].length, res=0;
-        int[][] memo= new int[m][n];
-        for (int i=0; i<m; i++)
-            for (int j=0; j<n; j++)
-                res=Math.max(res, dfs(matrix, i, j, memo, -1));
+        int m = matrix.length;
+        int n = matrix[0].length;
+        
+        boolean visited[][] = new boolean[m][n];
+        dp = new int[m][n];
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+               res = Math.max(res, solve(matrix,visited,i,j,m,n,-1,0));
+            }
+        }
         return res;
+        
     }
-    public int dfs(int[][] matrix, int i, int j, int[][] memo, int cur){
-        int m=matrix.length, n=matrix[0].length, res=0;
-        if (i<0 || i>=m || j<0 || j>=n || matrix[i][j]<=cur) return 0;
-        if (memo[i][j]!=0) return memo[i][j];
-        res=Math.max(res, dfs(matrix, i+1, j, memo, matrix[i][j]));
-        res=Math.max(res, dfs(matrix, i-1, j, memo, matrix[i][j]));
-        res=Math.max(res, dfs(matrix, i, j+1, memo, matrix[i][j]));
-        res=Math.max(res, dfs(matrix, i, j-1, memo, matrix[i][j]));
-        res++;
-        memo[i][j]=res;
-        return res;
+    int solve(int matrix[][],boolean visited[][],int i,int j,int m,int n,int prev,int count){
+        if(i>=m||j>=n||i<0||j<0||visited[i][j]==true||matrix[i][j]<=prev)return 0;
+        if(dp[i][j]!=0)return dp[i][j];
+        int temp = 0;
+            visited[i][j]=true;
+            temp = Math.max(temp,solve(matrix,visited,i+1,j,m,n,matrix[i][j],count+1));
+            temp = Math.max(temp,solve(matrix,visited,i,j+1,m,n,matrix[i][j],count+1));
+            temp = Math.max(temp,solve(matrix,visited,i-1,j,m,n,matrix[i][j],count+1));
+            temp = Math.max(temp,solve(matrix,visited,i,j-1,m,n,matrix[i][j],count+1));
+            visited[i][j]=false;
+        return dp[i][j]=temp+1;
     }
 }
