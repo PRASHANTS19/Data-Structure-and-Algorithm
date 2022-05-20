76b37989afa6004207785709f3570ba5dffb4b48
Prashant sahu <95503105+PRASHANTS19@users.noreply.github.com>
2022-05-20 09:36:41 +0530

    Time: 0 ms (100.00%), Space: 42.9 MB (10.74%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.java
+++ 63-unique-paths-ii/63-unique-paths-ii.java
@@ -1,26 +1,26 @@
 class Solution {
+    int dp[][];
     public int uniquePathsWithObstacles(int[][] arr) {
         int m = arr.length;
         int n = arr[0].length;
+        dp = new int[m][n];
+        for(int i=0; i<m; i++)
+            Arrays.fill(dp[i],-1);
         
-        int dp[][] = new int[m][n];
-        
-        for(int x[] : dp){
-            Arrays.fill(x,-1);
-        }
-        return solve(arr,m-1,n-1,dp);
-        
-        
+        return solve(arr,m-1,n-1,new boolean[m][n]);
     }
-    int solve(int arr[][],int m,int n,int dp[][]){
-        if(m<0 || n<0 || arr[m][n]==1)return 0;
+    int solve(int[][] arr,int m,int n,boolean visited[][]){
+        if(m<0||n<0||arr[m][n]==1||visited[m][n]==true)return 0;
+        
         if(m==0 && n==0)return 1;
         
         if(dp[m][n]!=-1)return dp[m][n];
         
-        int x = solve(arr,m-1,n,dp);
-        int y = solve(arr,m,n-1,dp);
+        visited[m][n]=true;
+        int up = solve(arr,m-1,n,visited);
+        int left = solve(arr,m,n-1,visited);
+        visited[m][n]=false;
         
-        return dp[m][n]=x+y;
+        return dp[m][n]=up+left;
     }
 }
