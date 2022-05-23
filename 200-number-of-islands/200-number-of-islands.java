43ac6aeaeddd4daaab1c3fdc794800e69a04c63d
Prashant sahu <95503105+PRASHANTS19@users.noreply.github.com>
2022-05-23 23:27:03 +0530

    Time: 9 ms (22.32%), Space: 57 MB (56.73%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.java
+++ 200-number-of-islands/200-number-of-islands.java
@@ -1,29 +1,52 @@
 class Solution {
-    public int numIslands(char[][] arr) {
-        int r = arr.length;
-        int c = arr[0].length;
-        
-        boolean visited[][] = new boolean[r][c];
-        int count = 0;
-        for(int i=0; i<r; i++){
-            for(int j=0; j<c; j++){
-                if(arr[i][j]=='1' && visited[i][j]==false){
-                    dfs(arr,visited,i,j,r,c);
-                    count++;
+    public int numIslands(char[][]arr) {
+        int m = arr.length;
+        int n = arr[0].length;
+        boolean visited[][] = new boolean[m][n];
+        int res = 0;
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(arr[i][j]=='1' && visited[i][j]==false)
+                {
+                    bfs(arr,i,j,m,n,visited);
+                    res++;
                 }
             }
         }
-        return count;
+        return res;
     }
-    void dfs(char[][] arr,boolean visited[][],int i,int j,int r,int c){
-        if(i<0||j<0||i>=r||j>=c||arr[i][j]=='0')return;
+    
+    void bfs(char[][] arr,int i,int j,int m,int n,boolean visited[][]){
+        if(i>=m||j>=n||i<0||j<0||visited[i][j]==true||arr[i][j]=='0')return;
         
-        if(arr[i][j]=='1' && visited[i][j]==false){
-            visited[i][j]=true;
-            dfs(arr,visited,i+1,j,r,c);
-            dfs(arr,visited,i,j+1,r,c);
-            dfs(arr,visited,i-1,j,r,c);
-            dfs(arr,visited,i,j-1,r,c);
+        int dir[][] = {{1,0},{-1,0},{0,1},{0,-1}};
+        
+        Queue<int[]>q = new LinkedList<>();
+        q.add(new int[]{i,j});
+        
+        visited[i][j]=true;
+        
+        int count = 0;
+        while(q.isEmpty()!=true){
+            int size = q.size();
+            for(int k=0; k<size; k++)
+            {
+                int[] temp = q.remove();
+                int x = temp[0];
+                int y = temp[1];
+                
+                for(int l=0; l<4; l++){
+                    int nextx = x+dir[l][0];
+                    int nexty = y+dir[l][1];
+                    if(nextx>=m||nexty>=n||nextx<0||nexty<0||visited[nextx][nexty]==true||arr[nextx][nexty]=='0')continue;
+                    else {
+                        visited[nextx][nexty]=true;
+                        q.add(new int[]{nextx,nexty});
+                    }
+                }
+            }
         }
+        
+        
     }
 }
