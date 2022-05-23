e63379e881a609486465dbb9e3f09d0a68ba6260
Prashant sahu <95503105+PRASHANTS19@users.noreply.github.com>
2022-05-23 21:47:45 +0530

    Time: 38 ms (44.40%), Space: 43.2 MB (54.96%) - LeetHub

---
+++ 133-clone-graph/133-clone-graph.java
@@ -0,0 +1,48 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> neighbors;
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val) {
+        val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
+*/
+
+class Solution {
+    public Node cloneGraph(Node node) {
+        if(node==null)return node;
+        boolean visited[] = new boolean[110];
+        Queue<Node> q = new LinkedList<>();
+        q.add(node);
+        visited[node.val]=true;
+        
+        HashMap<Integer,Node> map = new HashMap<>();
+        map.put(node.val,new Node(node.val));
+        
+        while(q.isEmpty()!=true){
+            int size = q.size();
+            for(int i=0; i<size; i++){
+                Node temp = q.remove();
+                
+                for(Node x : temp.neighbors){
+                    if(map.containsKey(x.val)!=true){
+                        map.put(x.val,new Node(x.val, new ArrayList<Node>()));
+                        q.add(x);
+                    }
+                    map.get(temp.val).neighbors.add(map.get(x.val));
+                }
+            }
+        }
+        return map.get(node.val);
+    }
+}
