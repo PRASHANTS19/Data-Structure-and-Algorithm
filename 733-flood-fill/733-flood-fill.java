804b837e7c366e42644ba11af52bc4db81dc424a
Prashant sahu <95503105+PRASHANTS19@users.noreply.github.com>
2022-05-28 03:26:45 +0530

    Time: 2 ms (30.72%), Space: 48.3 MB (29.69%) - LeetHub

--- 733-flood-fill/733-flood-fill.java
+++ 733-flood-fill/733-flood-fill.java
@@ -1,24 +1,36 @@
 class Solution {
     public int[][] floodFill(int[][] arr, int i, int j, int newColor) {
-        int n = arr.length;
-        int m = arr[0].length;
-        boolean visited[][] = new boolean[n][m];
-        int src = arr[i][j];
-        dfs(arr,src,i,j,n,m,newColor,visited);
+        int m = arr.length;
+        int n = arr[0].length;
         
-        return arr;
-    }
-void dfs(int[][] arr,int src,int i,int j,int n,int m,int newColor,boolean[][]visited){
-        if(i<0 || j<0 || i>=n || j>=m)return;
-        if(src!=0 && arr[i][j]==0) return;
+        int color = arr[i][j];
+        int dir[][] = {{1,0},{0,1},{-1,0},{0,-1}};
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[]{i,j});
+        boolean visited[][] = new boolean[m][n];
+        visited[i][j]=true;
         
-        if( arr[i][j]==src && visited[i][j]==false){
-          arr[i][j]=newColor;
-          visited[i][j]=true;
-        dfs(arr,src,i+1,j,n,m,newColor,visited);
-        dfs(arr,src,i-1,j,n,m,newColor,visited);
-        dfs(arr,src,i,j+1,n,m,newColor,visited);
-        dfs(arr,src,i,j-1,n,m,newColor,visited);
+        while(q.isEmpty()!=true){
+            int size = q.size();
+            for(int k=0; k<size; k++){
+                int[] temp = q.remove();
+                int x = temp[0];
+                int y = temp[1];
+                arr[x][y]=newColor;
+                visited[x][y]=true;
+                for(int v=0; v<4; v++){
+                    int nextx = x+dir[v][0];
+                    int nexty = y+dir[v][1];
+                    
+                    if(nextx<0||nextx>=m||nexty<0||nexty>=n||visited[nextx][nexty])continue;
+                    else{
+                        if(arr[nextx][nexty]==color)
+                            q.add(new int[]{nextx,nexty});
+                    }
+                }
+            }
         }
+        return arr;
+        
     }
 }
