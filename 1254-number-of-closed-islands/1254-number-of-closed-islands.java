9ab675fc6848042d44be6b25c853ffdba4651def
Prashant sahu <95503105+PRASHANTS19@users.noreply.github.com>
2022-05-28 17:08:19 +0530

    Time: 2 ms (89.89%), Space: 47.5 MB (18.81%) - LeetHub

---
+++ 1254-number-of-closed-islands/1254-number-of-closed-islands.java
@@ -0,0 +1,29 @@
+class Solution {
+    boolean visited[][];
+    public int closedIsland(int[][] arr) {
+        int m  = arr.length;
+        int n = arr[0].length;
+        visited= new boolean[m][n];
+        int ans = 0;
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(arr[i][j]==0&&visited[i][j]==false){
+                   if(dfs(arr,i,j,m,n,visited))ans++;
+                }
+            }
+        }
+        return ans;
+    }
+    boolean dfs(int arr[][],int i,int j,int m,int n,boolean visited[][]){
+        if(i>=m||j>=n||i<0||j<0)return false;
+        if(arr[i][j]==1||visited[i][j])return true;
+        
+        visited[i][j]=true;
+        boolean a = dfs(arr,i+1,j,m,n,visited);
+        boolean b = dfs(arr,i,j+1,m,n,visited);
+        boolean c = dfs(arr,i,j-1,m,n,visited);
+        boolean d = dfs(arr,i-1,j,m,n,visited);
+        
+        return a&&b&&c&&d;
+    }
+}
