fcab76b832a7bdd4b60b3258b7b8b7ee70aa112f
Prashant sahu <95503105+PRASHANTS19@users.noreply.github.com>
2022-05-30 12:12:15 +0530

    Time: 197 ms (24.72%), Space: 99.5 MB (33.83%) - LeetHub

--- 365-water-and-jug-problem/365-water-and-jug-problem.java
+++ 365-water-and-jug-problem/365-water-and-jug-problem.java
@@ -1,29 +1,32 @@
 class Solution {
-   public boolean canMeasureWater(int x, int y, int z) {
-    if (z < 0 || z > x + y) {
+    public boolean canMeasureWater(int j1, int j2, int targetCapacity) {
+        if(j1+j2==targetCapacity)return true;
+        if((j1+j2)<targetCapacity)return false;
+        
+        // if(j1%2==0 && j2%2==0 && targetCapacity%2!=0)return false;
+        
+        int total = j1+j2;
+        Queue<Integer> q = new LinkedList<>();
+        q.add(0);
+        HashSet<Integer> set = new HashSet<>();
+        
+        while(q.isEmpty()!=true){
+            int size = q.size();
+            for(int i=0; i<size; i++){
+                int temp = q.remove();
+                if(temp==targetCapacity)return true;
+                
+                if((temp+j1)<=total && set.add(temp+j1)==true)
+                    q.add(temp+j1);
+                if((temp+j2)<=total && set.add(temp+j2)==true)
+                    q.add(temp+j2);
+                if((temp-j1)>=0 && set.add(temp-j1)==true)
+                    q.add(temp-j1);
+                if((temp-j2)>=0 && set.add(temp-j2)==true)
+                    q.add(temp-j1);
+            }
+        }
         return false;
+        
     }
-    Set<Integer> set = new HashSet<>();
-    Queue<Integer> q = new LinkedList<>();
-    q.offer(0);
-    while (!q.isEmpty()) {
-        int n = q.poll();
-        if (n + x <= x + y && set.add(n + x)) {
-            q.offer(n + x);
-        } 
-        if (n + y <= x + y && set.add(n + y)) {
-            q.offer(n + y);
-        } 
-        if (n - x >= 0 && set.add(n - x)) {
-            q.offer(n - x);
-        } 
-        if (n - y >= 0 && set.add(n - y)) {
-            q.offer(n - y);
-        }
-        if (set.contains(z)) {
-            return true;
-        }
-    }
-    return false;
-}
 }
