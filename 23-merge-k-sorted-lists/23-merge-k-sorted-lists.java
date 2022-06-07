22aaba60ebe4dda6653bd2e47cece39eb53bc5cc
Prashant sahu <95503105+PRASHANTS19@users.noreply.github.com>
2022-06-07 12:54:04 +0530

    Time: 8 ms (46.10%), Space: 47.6 MB (35.76%) - LeetHub

--- 23-merge-k-sorted-lists/23-merge-k-sorted-lists.java
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.java
@@ -1,30 +1,42 @@
-
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
-   public static ListNode mergeKLists(ListNode[] lists){
-    return partion(lists,0,lists.length-1);
-}
-
-public static ListNode partion(ListNode[] lists,int s,int e){
-    if(s==e)  return lists[s];
-    if(s<e){
-        int q=(s+e)/2;
-        ListNode l1=partion(lists,s,q);
-        ListNode l2=partion(lists,q+1,e);
-        return merge(l1,l2);
-    }else
-        return null;
-}
-
-//This function is from Merge Two Sorted Lists.
-public static ListNode merge(ListNode l1,ListNode l2){
-    if(l1==null) return l2;
-    if(l2==null) return l1;
-    if(l1.val<l2.val){
-        l1.next=merge(l1.next,l2);
-        return l1;
-    }else{
-        l2.next=merge(l1,l2.next);
-        return l2;
+    public ListNode mergeKLists(ListNode[] arr) {
+        int k = arr.length;
+        PriorityQueue<pair>pq = new PriorityQueue<>((p,q)->p.val-q.val);
+        for(int i=0; i<k; i++){
+            if(arr[i]!=null)
+                pq.add(new pair(arr[i].val,arr[i]));
+        }
+        ListNode start = new ListNode(-1);
+        ListNode head = start;
+        while(pq.isEmpty()!=true){
+            pair temp = pq.remove();
+            ListNode x = temp.node;
+            start.next = x;
+            x = x.next;
+            start = start.next;
+            start.next = null;
+            if(x!=null){
+                pq.add(new pair(x.val,x));
+            }
+        }
+        return head.next;
     }
 }
+class pair{
+    int val;
+    ListNode node;
+    pair(int v,ListNode n){
+        val = v;
+        node = n;
+    }
 }
