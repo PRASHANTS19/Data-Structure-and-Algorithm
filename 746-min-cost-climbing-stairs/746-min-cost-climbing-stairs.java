aa1d82e0882ccbf171711e4ed1729c49593ae49e
Prashant sahu <95503105+PRASHANTS19@users.noreply.github.com>
2022-06-09 10:23:15 +0530

    Time: 2 ms (27.79%), Space: 43.7 MB (45.64%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.java
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.java
@@ -1,22 +1,28 @@
 class Solution {
-    int dp[];
+    // int dp[];
     public int minCostClimbingStairs(int[] arr) {
         int n = arr.length;
-        dp = new int[n];
-        Arrays.fill(dp,-1);
-        int x = solve(arr,n-1);
-        Arrays.fill(dp,-1);
-        int y = solve(arr,n-2);
-        int res = Math.min(x,y);
+        int dp[] = new int[n];
         
-        return res;
-    }
-    int solve(int arr[],int n){
-        if(n<0)return 0;
-        if(dp[n]!=-1)return dp[n];
-        int one = arr[n]+solve(arr,n-1);
-        int two = arr[n]+solve(arr,n-2);
+        if(n==2)return Math.min(arr[0],arr[1]);
         
-        return dp[n]=Math.min(one,two);
+        dp[0] = arr[0];
+        dp[1] = arr[1];
+        
+        for(int i=2; i<n; i++){
+            dp[i] = arr[i]+Math.min(dp[i-1],dp[i-2]);
+        }
+        
+        return Math.min(dp[n-1],dp[n-2]);
+        
+        
     }
+//     int solve(int arr[],int n){
+//         if(n<0)return 0;
+//         if(dp[n]!=-1)return dp[n];
+//         int one = arr[n]+solve(arr,n-1);
+//         int two = arr[n]+solve(arr,n-2);
+        
+//         return dp[n]=Math.min(one,two);
+//     }
 }
