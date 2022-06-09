6102fbbf9c2fa3b2102ad64188a92523e4b72e7c
Prashant sahu <95503105+PRASHANTS19@users.noreply.github.com>
2022-06-09 15:21:03 +0530

    Added solution - LeetHub

--- 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.java
+++ 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.java
@@ -51,30 +51,26 @@ class Solution
     static int dp[][];
     static int knapSack(int W, int wt[], int val[], int n) 
     { 
-         // your code here 
-        //  return solve(val,wt,W,n-1);
-         dp = new int[n+1][W+1];
+         // your code here
+         dp = new int[W+1][n];
+         
+         for(int i=0; i<W+1; i++){
+             Arrays.fill(dp[i],-1);
+         }
+         
+         return solve(val,wt,W,n-1);
          
      
-        for(int i=0; i<n+1; i++){
-            for(int j=0; j<W+1; j++){
-                if(i==0||j==0)
-                    dp[i][j]=0;
-                else if(wt[i-1]<=j)
-                    dp[i][j] = Math.max(val[i-1]+dp[i-1][j-wt[i-1]],dp[i-1][j]);
-                else 
-                    dp[i][j]=dp[i-1][j];
-            }
-        }
-        return dp[n][W];
-         
-         
-         
-         
+        
          
     } 
     static int solve(int value[],int weight[],int W,int n){
-        if(n<0 || W<=0)return 0;
+        // if(n<0 || W<=0)return 0;
+        
+        if(n==0){
+            if(weight[0]<=W)return value[0];
+            else return 0;
+        }
         
         if(dp[W][n]!=-1)return dp[W][n];
         
