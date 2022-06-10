660cb5e7403d8171735bcd13842b99f9d4a5b7b6
Prashant sahu <95503105+PRASHANTS19@users.noreply.github.com>
2022-06-10 11:41:09 +0530

    Time: 67 ms (15.25%), Space: 55 MB (17.59%) - LeetHub

--- 322-coin-change/322-coin-change.java
+++ 322-coin-change/322-coin-change.java
@@ -1,30 +1,26 @@
 class Solution {
     int dp[][];
     public int coinChange(int[] arr, int amount) {
-        int n =arr.length;
-        
+        int n = arr.length;
         dp = new int[n][amount+1];
-        
         for(int i=0; i<n; i++){
             Arrays.fill(dp[i],-1);
         }
-        
-        int ans = solve(arr,amount,n-1);
-        if(ans>=(int)1e9)return -1;
-        return ans;
+        int ans = solve(arr,n-1,amount); 
+        return ans==(int)1e9?-1:ans;
     }
-    
-    int solve(int arr[],int amount,int n){
-        if(amount==0)return 0;
-        if(amount<0 || n<0)return (int)1e9;
-        
+    int solve(int arr[],int n,int amount){
+        if(n==0){
+            if(amount%arr[0]==0)return amount/arr[0];
+            else return (int)1e9;
+        }
         if(dp[n][amount]!=-1)return dp[n][amount];
         
-        int notpick = 0+solve(arr,amount,n-1);
-        int pick = (int)1e9;
-        if(amount>=arr[n])
-            pick = 1+solve(arr,amount-arr[n],n);
-        
-        return dp[n][amount]=Math.min(notpick,pick);
+        int notselect = 0+solve(arr,n-1,amount);
+        int select = (int)1e9;
+        if(amount>=arr[n]){
+            select = 1+solve(arr,n,amount-arr[n]);
+        }
+        return dp[n][amount]=Math.min(select,notselect);
     }
 }
