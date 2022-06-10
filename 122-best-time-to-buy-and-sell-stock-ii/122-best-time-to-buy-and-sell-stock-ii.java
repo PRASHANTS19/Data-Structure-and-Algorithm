99affb521010d83c45661e4dfc50cf13340fd26e
Prashant sahu <95503105+PRASHANTS19@users.noreply.github.com>
2022-06-10 17:00:19 +0530

    Time: 10 ms (6.06%), Space: 47.7 MB (5.24%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.java
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,16 +1,27 @@
 class Solution {
+    int dp[][];
     public int maxProfit(int[] arr) {
-        int buy = arr[0];
-        int profit =0;
+     int n = arr.length;
+     dp = new int[n][2];
+      for(int i=0; i<n; i++){
+          Arrays.fill(dp[i],-1);
+      } 
+     return solve(arr,0,0);   
+    }
+    int solve(int arr[],int ind, int buy){
+        if(ind==arr.length)return 0;
+        if(dp[ind][buy]!=-1)return dp[ind][buy];
+        int select = 0;
+        int notselect = 0;
         
-        for(int i=1; i<arr.length; i++){
-            if(arr[i]<buy)
-                buy = arr[i];
-            else if(arr[i]>buy){
-                profit += arr[i]-buy;
-                buy = arr[i];
-            }
+        if(buy==0){
+            notselect = solve(arr,ind+1,0);
+            select = -arr[ind]+solve(arr,ind+1,1);
         }
-        return profit;
+        if(buy==1){
+            notselect = solve(arr,ind+1,1);
+            select = arr[ind]+solve(arr,ind+1,0);
+        }
+        return dp[ind][buy]=Math.max(notselect,select);
     }
 }
