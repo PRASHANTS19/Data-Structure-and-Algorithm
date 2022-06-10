bf438446a0e3b7e7c96e1a30123ba81c0145edf3
Prashant sahu <95503105+PRASHANTS19@users.noreply.github.com>
2022-06-10 11:48:48 +0530

    Time: 41 ms (30.97%), Space: 54.6 MB (18.57%) - LeetHub

--- 322-coin-change/322-coin-change.java
+++ 322-coin-change/322-coin-change.java
@@ -3,11 +3,30 @@ class Solution {
     public int coinChange(int[] arr, int amount) {
         int n = arr.length;
         dp = new int[n][amount+1];
+        
         for(int i=0; i<n; i++){
-            Arrays.fill(dp[i],-1);
+            Arrays.fill(dp[i],(int)1e9);
         }
-        int ans = solve(arr,n-1,amount); 
-        return ans==(int)1e9?-1:ans;
+        
+        for(int i=0; i<=amount; i++){
+            if(i%arr[0]==0)
+                dp[0][i]=i/arr[0];
+        }
+        
+        for(int ind=1; ind<n; ind++){
+            for(int am=0; am<=amount; am++){
+             
+                int notselect = 0+dp[ind-1][am];
+                int select = (int)1e9;
+                if(am>=arr[ind]){
+                    select = 1+dp[ind][am-arr[ind]];
+                }
+                dp[ind][am]=Math.min(select,notselect);
+            }
+        }
+        int ans = dp[n-1][amount];
+        if(ans>=(int)1e9)return -1;
+        return ans;
     }
     int solve(int arr[],int n,int amount){
         if(n==0){
