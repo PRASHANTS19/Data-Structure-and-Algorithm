e58824a158ea12609210e90c8ce58fbd6f56f0a5
Prashant sahu <95503105+PRASHANTS19@users.noreply.github.com>
2022-06-11 18:45:41 +0530

    Time: 272 ms (18.99%), Space: 67.6 MB (58.59%) - LeetHub

--- 55-jump-game/55-jump-game.java
+++ 55-jump-game/55-jump-game.java
@@ -1,15 +1,22 @@
 class Solution {
-    public boolean canJump(int[] nums) {
-    int n = nums.length;
-    if(n==1)return true;
-    int max = -1;
-    for(int i=0; i<n; i++){
-        max = Math.max(max,nums[i]);
-        if(i==n-1)return true;
-        if(max==0)break;
-        max--;
-    }
-    return false;
+    int dp[];
+    public boolean canJump(int[] arr) {
+        dp = new int[arr.length];
+        Arrays.fill(dp,-1);
         
+       return solve(arr,0); 
+    }
+    boolean solve(int arr[],int start){
+        if(start>=arr.length-1)return true;
+        
+        if(dp[start]!=-1)return dp[start]==0?false:true;
+        
+        boolean ans = false;
+        for(int jump=arr[start]; jump>0; jump--){
+            ans = ans||solve(arr,start+jump);
+        }
+        if(ans)dp[start]=1;
+        else dp[start]=0;
+        return ans;
     }
 }
