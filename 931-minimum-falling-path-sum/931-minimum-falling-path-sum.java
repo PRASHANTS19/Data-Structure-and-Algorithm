1c5b105b3c03804f0a65f360415513a7d1abc7f1
Prashant sahu <95503105+PRASHANTS19@users.noreply.github.com>
2022-06-12 17:43:36 +0530

    Time: 8 ms (23.00%), Space: 48.3 MB (12.93%) - LeetHub

--- 931-minimum-falling-path-sum/931-minimum-falling-path-sum.java
+++ 931-minimum-falling-path-sum/931-minimum-falling-path-sum.java
@@ -1,8 +1,8 @@
 class Solution {
-    int dp[][];
+    
     public int minFallingPathSum(int[][] arr) {
         int n= arr.length;
-        dp = new int[n][n];
+        int dp[][] = new int[n][n];
         
         for(int i=0; i<n; i++){
             dp[n-1][i] = arr[n-1][i];
@@ -28,17 +28,17 @@ class Solution {
         return min;
     }
     
-    int solve(int arr[][],int n,int i,int j){
-        if(i<0||j<0||i>n-1||j>n-1)return (int)1e9;
-        if(i==n-1){
-            return arr[i][j];
-        }
-        if(dp[i][j]!=Integer.MAX_VALUE)return dp[i][j];
+//     int solve(int arr[][],int n,int i,int j){
+//         if(i<0||j<0||i>n-1||j>n-1)return (int)1e9;
+//         if(i==n-1){
+//             return arr[i][j];
+//         }
+//         if(dp[i][j]!=Integer.MAX_VALUE)return dp[i][j];
         
-        int x = solve(arr,n,i+1,j);
-        int y = solve(arr,n,i+1,j+1);
-        int z = solve(arr,n,i+1,j-1);
+//         int x = solve(arr,n,i+1,j);
+//         int y = solve(arr,n,i+1,j+1);
+//         int z = solve(arr,n,i+1,j-1);
         
-        return dp[i][j]=arr[i][j]+Math.min(Math.min(x,y),z);
-    }
+//         return dp[i][j]=arr[i][j]+Math.min(Math.min(x,y),z);
+//     }
 }
