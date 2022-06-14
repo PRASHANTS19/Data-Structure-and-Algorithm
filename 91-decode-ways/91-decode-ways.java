1d3de7c2a407ccf3ef60eb63c9969a7531bbae36
Prashant sahu <95503105+PRASHANTS19@users.noreply.github.com>
2022-06-14 12:41:32 +0530

    Time: 3 ms (35.90%), Space: 42.9 MB (17.58%) - LeetHub

---
+++ 91-decode-ways/91-decode-ways.java
@@ -0,0 +1,32 @@
+class Solution {
+    int dp[];
+    public int numDecodings(String s) {
+        if(s.charAt(0)=='0')return 0;
+        HashSet<String> set = new HashSet<>();
+        for(int i=1; i<=26; i++){
+            set.add(Integer.toString(i));
+        }
+        dp = new int[s.length()];
+        Arrays.fill(dp,-1);
+
+        return solve(s,0,set);
+    }
+    int solve(String s,int start,HashSet<String>set){
+        if(start==s.length())return 1;
+        if(s.charAt(start)=='0')return 0;
+        if(start==s.length()-1)return 1;
+        
+        if(dp[start]!=-1)return dp[start];
+        
+        String sub = s.substring(start,start+2);
+        int one = solve(s,start+1,set);
+        int two = 0;
+        if(set.contains(sub)){
+            two = solve(s,start+2,set);
+        }
+        
+        return dp[start]=one+two;
+        
+       
+    }
+}
