f77b35675d7366351fd4f878b9110869a9d80772
Prashant sahu <95503105+PRASHANTS19@users.noreply.github.com>
2022-06-15 12:07:24 +0530

    Time: 12 ms (79.22%), Space: 54.3 MB (27.84%) - LeetHub

--- 646-maximum-length-of-pair-chain/646-maximum-length-of-pair-chain.java
+++ 646-maximum-length-of-pair-chain/646-maximum-length-of-pair-chain.java
@@ -1,32 +1,18 @@
 class Solution {
-    public int findLongestChain(int[][] A) {
-       ArrayList<pair> list = new ArrayList<>();
-      for(int i=0; i<A.length; i++){
-          int f=A[i][0];
-          int s=A[i][1];
-          list.add(new pair(f,s));
-      }
-      Collections.sort(list);
-      int count=1;
-        int j=0;
-      for(int i=1; i<list.size(); i++){
-          if(list.get(j).second<list.get(i).first){
-              count++;
-              j=i;
-          }
-      }
-      return count;
-    }
-}
-class pair implements Comparable<pair>{
-    int first;
-    int second;
-
-    pair(int f,int s){
-        first = f;
-        second = s;
-    }
-    public int compareTo(pair o) {
-		return this.second-o.second;
+    public int findLongestChain(int[][] arr) {
+        Arrays.sort(arr,(p,q)->p[1]-q[1]);
+        
+        int count = 1;
+        int i=1,j=0;
+        
+        while(i<arr.length){
+            if(arr[i][0]>arr[j][1]){
+                count++;
+                j=i;
+            }
+            i++;
+        }
+        return count;
+        
     }
 }
