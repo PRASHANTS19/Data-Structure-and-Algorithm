3081249c5ce9e0d29568501820e9a45612d9be73
Prashant sahu <95503105+PRASHANTS19@users.noreply.github.com>
2022-06-15 18:18:24 +0530

    Time: 6 ms (88.66%), Space: 42.3 MB (87.29%) - LeetHub

--- 72-edit-distance/72-edit-distance.java
+++ 72-edit-distance/72-edit-distance.java
@@ -4,12 +4,34 @@ class Solution {
         
         int m = s1.length();
         int n = s2.length();
-        dp = new int[m][n];
+        // dp = new int[m][n];
+        // for(int i=0; i<m; i++)
+        //     Arrays.fill(dp[i],-1);
         
-        for(int i=0; i<m; i++)
-            Arrays.fill(dp[i],-1);
+        //for tabulation we have to follow 1base indexing as basecase is <0 which is not practical in zero base.
+        dp = new int[m+1][n+1];
+        for(int i=0; i<=m; i++)
+            dp[i][0]=i;
+        for(int i=0; i<=n; i++)
+            dp[0][i]=i;
         
-        return solve(s1,s2,m-1,n-1);
+        for(int i=1; i<=m; i++){
+            for(int j=1; j<=n; j++){
+                int insert=(int)1e9,delete=(int)1e9,replace=(int)1e9;
+        
+                if(s1.charAt(i-1)==s2.charAt(j-1))
+                    dp[i][j]= 0+dp[i-1][j-1];
+                else{
+                    insert =dp[i][j-1];
+                    delete = dp[i-1][j];
+                    replace = dp[i-1][j-1];
+                    dp[i][j]=1+Math.min(insert,Math.min(delete,replace));
+                }
+                 
+            }
+        }
+        return dp[m][n];
+        // return solve(s1,s2,m-1,n-1);
     }
     int solve(String s1,String s2,int m,int n){
         if(m<0)return n+1;
