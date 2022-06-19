80ef47c234e7d2ea3785d02b00470f152070185e
Prashant sahu <95503105+PRASHANTS19@users.noreply.github.com>
2022-06-19 14:46:36 +0530

    Time: 10 ms (57.56%), Space: 42.9 MB (71.38%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.java
+++ 567-permutation-in-string/567-permutation-in-string.java
@@ -1,33 +1,40 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
+       
         int n1 = s1.length();
         int n2 = s2.length();
+        
         if(n1>n2)return false;
-        
-        int arr1[] = new int[27];
+        int arr[] = new int[27];
         for(int i=0; i<n1; i++){
-            arr1[s1.charAt(i)-'a']++;
+            arr[s1.charAt(i)-'a']++;
         }
-        int arr2[] = new int[27];
         
+        int dummy[] = new int[27];
         for(int i=0; i<n1; i++){
-            arr2[s2.charAt(i)-'a']++;
+            dummy[s2.charAt(i)-'a']++;
         }
-        int j=0;  boolean flag = true;
-        for(int i=n1; i<=n2; i++){
-            for(int k=0; k<27; k++){
-                if(arr1[k]!=arr2[k]){
-                    flag = false;
-                    break;
-                }
+        boolean flag = true;
+        for(int i=0; i<27; i++){
+            if(arr[i]!=dummy[i]){
+                flag = false;
+                break;
             }
-            if(flag == true)return true;
-            flag = true;
-            if(i==n2)break;
-            arr2[s2.charAt(i)-'a']++;
-            arr2[s2.charAt(j++)-'a']--;
-            
         }
-        return false; 
+        if(flag)return true;
+        int j = 0;
+        for(int i=n1; i<n2; i++){
+            dummy[s2.charAt(i)-'a']++;
+            dummy[s2.charAt(j++)-'a']--;
+            flag = true;
+        for(int k=0; k<27; k++){
+            if(arr[k]!=dummy[k]){
+                flag = false;
+                break;
+            }
+        }
+        if(flag)return true;
+        }
+    return flag;
     }
 }
