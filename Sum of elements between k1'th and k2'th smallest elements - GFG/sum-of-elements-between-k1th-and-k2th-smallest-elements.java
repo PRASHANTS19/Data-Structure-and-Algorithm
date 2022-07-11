5b47975193bc07df60dbec7d4d3074095e273ba9
Prashant sahu <95503105+PRASHANTS19@users.noreply.github.com>
2022-07-11 20:41:32 +0530

    Added solution - LeetHub

---
+++ Sum of elements between k1'th and k2'th smallest elements - GFG/sum-of-elements-between-k1th-and-k2th-smallest-elements.java
@@ -0,0 +1,51 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+	public static void main(String[] args) throws IOException
+	{
+	        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t =
+            Integer.parseInt(br.readLine().trim());
+        while(t-->0)
+        {
+            long n = Long.parseLong(br.readLine().trim());
+            long a[] = new long[(int)n];
+            String inputLine[] = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                a[i] = Long.parseLong(inputLine[i]);
+            }
+            
+            StringTokenizer stt = new StringTokenizer(br.readLine());
+            long k1 = Long.parseLong(stt.nextToken());
+            long k2 = Long.parseLong(stt.nextToken());
+            
+            Solution obj = new Solution();
+            System.out.println( obj.sumBetweenTwoKth(a, n, k1, k2) );
+            
+        }
+	}
+}// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Solution { 
+    public static long sumBetweenTwoKth(long A[], long N, long k1, long k2)
+    {
+        // Your code goes here
+        Arrays.sort(A);
+        long sum = 0;
+        for(int i =(int)k1; i<(int)k2-1; i++){
+            sum += A[i];
+        }
+        return sum;
+    }
+    
+}
