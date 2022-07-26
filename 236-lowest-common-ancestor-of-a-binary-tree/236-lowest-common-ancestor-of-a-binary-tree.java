41b3303bd708b1eb1cbb364d8547cd1b92c63e4c
Prashant sahu <95503105+PRASHANTS19@users.noreply.github.com>
2022-07-26 13:14:22 +0530

    Time: 9 ms (66.53%), Space: 47.6 MB (29.25%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.java
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.java
@@ -14,24 +14,23 @@ class Solution {
         solve(root,p, q);
         return res;
     }
+    
     boolean solve(TreeNode root, TreeNode p, TreeNode q){
         if(root==null)return false;
-        if(res!=null)return true;
+        if(res!=null)return true; //to reduce the calls if we get the ansewer.
         
         boolean left = solve(root.left,p,q);
         boolean right = solve(root.right,p,q);
         
         if((root==p||root==q)&&(left==true||right==true)){
-             if(res==null){
+             if(res==null)
                 res = root;
-            }
         }
-        
         if(root==p || root==q)return true;
+        
         if(left==true && right==true){
-            if(res==null){
+            if(res==null)
                 res = root;
-            }
         }
         return left||right;
     }
