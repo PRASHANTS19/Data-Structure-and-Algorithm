7425d588669b4e15b63c74cf35798046adadcb55
Prashant sahu <95503105+PRASHANTS19@users.noreply.github.com>
2022-07-29 17:20:50 +0530

    Time: 0 ms (100.00%), Space: 41.8 MB (50.89%) - LeetHub

---
+++ 1221-split-a-string-in-balanced-strings/1221-split-a-string-in-balanced-strings.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int balancedStringSplit(String s) {
+        
+        int r=0;
+        int l=0;
+        int count = 0;
+        for(int i=0; i<s.length(); i++){
+            if(s.charAt(i)=='R')r++;
+            else l++;
+            
+            if(r!=0 && r==l){
+                count++;
+                r=0; l=0;
+            }
+        }
+        return count;
+    }
+}
