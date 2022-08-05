61715ea7d50337a7a713903e323aaeac03434d0e
Prashant sahu <95503105+PRASHANTS19@users.noreply.github.com>
2022-08-05 12:24:52 +0530

    Time: 2 ms (65.18%), Space: 42.2 MB (13.55%) - LeetHub

--- 377-combination-sum-iv/377-combination-sum-iv.java
+++ 377-combination-sum-iv/377-combination-sum-iv.java
@@ -1,21 +1,20 @@
 class Solution {
-    int dp[];
-    public int combinationSum4(int[] arr, int target) {
-        dp = new int[target+1];
-        
-      
-            Arrays.fill(dp,-1);
-      
-        return solve(arr,target);
-    }
-    int solve(int arr[],int target){
-        if(target==0)return 1;
-        if(dp[target]!=-1)return dp[target];
-        int res = 0;
-        for(int i=0; i<arr.length; i++){
-            if(target>=arr[i])
-                res += solve(arr,target-arr[i]);
+    int dp[][];
+    public int combinationSum4(int[] nums, int target) {
+        dp = new int[nums.length][target+1];
+        for(int i=0; i<nums.length; i++){
+            Arrays.fill(dp[i],-1);
         }
-        return dp[target]=res;
+        return solve(nums,target,0);
+    }
+    int solve(int arr[],int target,int i){
+        if(target==0)return 1;
+        if(i==arr.length || target<0)return 0;
+        if(dp[i][target]!=-1)return dp[i][target];
+        
+        int select = solve(arr,target-arr[i],0);
+        int notselect = solve(arr,target,i+1);
+        
+        return dp[i][target]=select+notselect;
     }
 }
