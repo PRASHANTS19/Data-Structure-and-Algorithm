f15830fa2eb6837e7b01a90f0799e0cea61ee2c0
Prashant sahu <95503105+PRASHANTS19@users.noreply.github.com>
2022-11-08 09:45:21 +0530

    Time: 18 ms (5.10%), Space: 43.4 MB (15.22%) - LeetHub

--- 1544-make-the-string-great/1544-make-the-string-great.java
+++ 1544-make-the-string-great/1544-make-the-string-great.java
@@ -3,17 +3,13 @@ class Solution {
         Stack<Character> stack = new Stack<>();
         
         for(int i=0; i<s.length(); i++){
-            if(s.charAt(i)>=97 && stack.size()>0){
-                if((stack.peek()-'A')==(s.charAt(i)-'a')){
+            if(stack.size()>0 && (stack.peek()-'A')==(s.charAt(i)-'a')){
                     stack.pop();
                     continue;
-                }
             }
-            else if(s.charAt(i)<=90 && stack.size()>0){
-                if((stack.peek()-'a')==(s.charAt(i)-'A')){
+            if(stack.size()>0 && (stack.peek()-'a')==(s.charAt(i)-'A')){
                     stack.pop();
                     continue;
-                }
             }
             stack.push(s.charAt(i));
         }
