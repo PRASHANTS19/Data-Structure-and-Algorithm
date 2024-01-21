3299b78f7065146ee5d34537a81fb1570b636389
Prashant sahu <95503105+PRASHANTS19@users.noreply.github.com>
2024-01-21 20:27:54 +0530

    Time: 0 ms (100.00%), Space: 41.1 MB (23.47%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,32 +1,17 @@
 class Solution {
+    int dp[];
     public int rob(int[] arr) {
-        int n = arr.length;
-        
-        int dp[] = new int[n];
-        
-        if(n==1)return arr[0];
-        if(n==2)return Math.max(arr[0],arr[1]);
-        
-        dp[0] = arr[0];
-        dp[1] = Math.max(arr[0],arr[1]);
-        
-        for(int i=2; i<n; i++){
-            int select = arr[i]+dp[i-2];
-            int notselect = 0+dp[i-1];
-            
-            dp[i] = Math.max(select,notselect);
-        }
-        return Math.max(dp[n-1],dp[n-2]);
-        
+        dp = new int[arr.length];
+        Arrays.fill(dp,-1);
+        return solve(arr,0);
     }
-    
-    
-    int solve(int arr[],int n){
-        if(n<0)return 0;
+    int solve(int arr[],int i){
+        if(i>=arr.length)return 0;
         
-        int select = arr[n]+solve(arr,n-2);
-        int notselect = 0+solve(arr,n-1);
+        if(dp[i]!=-1)return dp[i];
+        int select = solve(arr,i+2)+arr[i];
+        int notselect = solve(arr, i+1);
         
-        return Math.max(select,notselect);
+        return dp[i]=Math.max(select,notselect);
     }
 }
