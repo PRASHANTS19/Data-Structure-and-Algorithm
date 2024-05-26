ae05a7711fc590801162618c9977ed5c6811ab8f
Prashant sahu <95503105+PRASHANTS19@users.noreply.github.com>
2024-05-27 01:06:05 +0530

    Time: 515 ms (16.49%), Space: 89.7 MB (16.13%) - LeetHub

--- 0552-student-attendance-record-ii/0552-student-attendance-record-ii.java
+++ 0552-student-attendance-record-ii/0552-student-attendance-record-ii.java
@@ -1,15 +1,30 @@
 class Solution {
+    int mod = (int)1e9+7;
+    long dp[][][];
     public int checkRecord(int n) {
-    int[] dp = { 1, 1, 0, 1, 0, 0 }; // init table for n = 1
-    for (int i = 2; i <= n; i++) // updating table for n = i
-        dp = new int[] { sum(dp, 0, 2), dp[0], dp[1], sum(dp, 0, 5), dp[3], dp[4] };
-    return sum(dp, 0, 5);       
-}                                   
-
-static int sum(int[] arr, int l, int h) {
-    int s = 0;  
-    for (int i = l; i <= h; i++) 
-        s = (s + arr[i]) % 1000000007;  
-    return s;                   
-} 
+        dp = new long[n+1][2][3];
+        
+        for(int i=0; i<n+1; i++){
+            for(int j=0; j<2; j++){
+                Arrays.fill(dp[i][j],-1);
+            }
+        }
+        
+        
+        return (int)solve(n,1,2)%mod;
+    }
+    long solve(int i, int a, int l){
+        if(i<=0)return 1;
+        if(dp[i][a][l]!=-1)return dp[i][a][l]%mod;
+        long aa=0,ll=0,pp=0;
+        if(a>0){
+            aa = solve(i-1,a-1,2)%mod;
+        }
+        if(l>0){
+            ll = solve(i-1,a,l-1)%mod;
+        }
+        pp = solve(i-1,a,2)%mod;
+        
+        return dp[i][a][l]=(aa%mod+ll%mod+pp%mod)%mod;
+    }
 }
